bbed93cb8953ff4ea7a02a43e06ad0c8476d7535
hrshdg8 <hrshdg8@gmail.com>
2020-01-25 18:09:28 +0530

    Added gameover and dead Piece Block

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -60,110 +60,133 @@ const Board: React.FC = () => {
     let [blackKilled, setBlackKilled] = React.useState(blackDeadPiece);
     let [turn, setTurn] = React.useState(Color.white);
     let [selectedPiece, setSelectedPiece] = React.useState({ x: -1, y: -1 });
-    function resolveTheme(color: Color) {
+    let [gameOver, setGameOver] = React.useState(false);
+    function resolveTheme(color: Color): { readonly [key: string]: string } {
         return color === Color.black ? dark : light;
     }
     function handleSquareClick(square: Square, x: number, y: number) {
-        if (square.piece) {
-            if (square.piece.color === turn) {
-                const coordinates: Coordinate[] = square.piece.onClick(x, y, board);
+        if (!gameOver)
+            if (square.piece) {
+                if (square.piece.color === turn) {
+                    let kingPos: Coordinate = { x: -1, y: -1 };
+                    board.forEach((row: Square[], _y: number) => {
+                        row.forEach((cell: Square, _x: number) => {
+                            cell.highlight = false;
+                            if (cell.piece?.color === turn && cell.piece instanceof King) {
+                                kingPos = { x: _x, y: _y };
+                            }
+                        })
+                    });
+                    const coordinates: Coordinate[] = square.piece.onClick(x, y, board);
+                    coordinates.forEach((coordinate: Coordinate) => {
+                        board[coordinate.y][coordinate.x].highlight = true;
+                    });
+                    setBoard([...board]);
+                    setSelectedPiece({ ...{ x: x, y: y } });
+                } else {
+                    if (selectedPiece.x !== -1 && square.highlight) {
+                        board.forEach((row: Square[], _y: number) => {
+                            row.forEach((cell: Square, _x: number) => {
+                                cell.highlight = false;
+                            })
+                        });
+                        const deadPiece = board[y][x].piece;
+                        setGameOver(deadPiece instanceof King);
+                        if (turn === Color.white) {
+                            const blackDead = blackKilled;
+                            if (deadPiece !== null) {
+                                blackDead.push(deadPiece);
+                            }
+                            setBlackKilled([...blackDead]);
+                        } else {
+                            const whiteDead = whiteKilled;
+                            if (deadPiece !== null) {
+                                whiteDead.push(deadPiece);
+                            }
+                            setWhiteKilled([...whiteDead]);
+                        }
+                        board[y][x].piece = board[selectedPiece.y][selectedPiece.x].piece;
+                        board[selectedPiece.y][selectedPiece.x].piece = null;
+                        setSelectedPiece({ x: -1, y: -1 });
+                        setBoard([...board]);
+
+                        setTurn(turn === Color.white ? Color.black : Color.white);
+                    }
+                }
+            } else if (square.highlight) {
                 board.forEach((row: Square[], _y: number) => {
                     row.forEach((cell: Square, _x: number) => {
                         cell.highlight = false;
                     })
                 });
-                coordinates.forEach((coordinate: Coordinate) => {
-                    board[coordinate.y][coordinate.x].highlight = true;
-                });
+                board[y][x].piece = board[selectedPiece.y][selectedPiece.x].piece;
+                board[selectedPiece.y][selectedPiece.x].piece = null;
+                setSelectedPiece({ x: -1, y: -1 });
                 setBoard([...board]);
-                setSelectedPiece({ ...{ x: x, y: y } });
-            } else {
-                if (selectedPiece.x !== -1 && square.highlight) {
-                    board.forEach((row: Square[], _y: number) => {
-                        row.forEach((cell: Square, _x: number) => {
-                            cell.highlight = false;
-                        })
-                    });
-                    const deadPiece = board[y][x].piece;
-                    if (turn === Color.white) {
-                        const blackDead = blackKilled;
-                        if (deadPiece !== null) {
-                            blackDead.push(deadPiece);
-                        }
-                        setBlackKilled([...blackDead]);
-                    } else {
-                        const whiteDead = whiteKilled;
-                        if (deadPiece !== null) {
-                            whiteDead.push(deadPiece);
-                        }
-                        setWhiteKilled([...whiteDead]);
-                    }
-                    board[y][x].piece = board[selectedPiece.y][selectedPiece.x].piece;
-                    board[selectedPiece.y][selectedPiece.x].piece = null;
-                    setSelectedPiece({ x: -1, y: -1 });
-                    setBoard([...board]);
-
-                    setTurn(turn === Color.white ? Color.black : Color.white);
-                }
+                setTurn(turn === Color.white ? Color.black : Color.white);
             }
-        } else if (square.highlight) {
-            board.forEach((row: Square[], _y: number) => {
-                row.forEach((cell: Square, _x: number) => {
-                    cell.highlight = false;
-                })
-            });
-            board[y][x].piece = board[selectedPiece.y][selectedPiece.x].piece;
-            board[selectedPiece.y][selectedPiece.x].piece = null;
-            setSelectedPiece({ x: -1, y: -1 });
-            setBoard([...board]);
-            setTurn(turn === Color.white ? Color.black : Color.white);
-        }
     }
     return (
         <>
-            <table className={classNames.board}>
-                <tbody>
-                    {
-                        board.map((row: Square[], y: number) => {
-                            return (
-                                <tr key={y}>
-                                    {
-                                        row.map((square: Square, x: number) => {
-                                            return <td key={x} className={classNames.cell}>
-                                                <div className={classNames.square + " " + resolveTheme(square.color).squareColor + " " + (square.highlight ? classNames.highlight : '') + " " + (selectedPiece.x === x && selectedPiece.y === y ? classNames.currentSelection : "")} onClick={() => { handleSquareClick(square, x, y) }}>
-                                                    {square.piece ? square.piece.render() : null}
-                                                </div>
-                                            </td>
-                                        })
-                                    }
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-            <table>
-                <tbody>
-                    <tr>
-                        {blackKilled.map((dead: Piece, index: number) => {
-                            return <td key={index}>
-                                {dead.render()}
-                            </td>
-                        })}
-                    </tr>
-                </tbody>
-            </table>
-            <table>
-                <tbody>
-                    <tr>
-                        {whiteKilled.map((dead: Piece, index: number) => {
-                            return <td key={index}>
-                                {dead.render()}
-                            </td>
-                        })}
-                    </tr>
-                </tbody>
-            </table>
+            <div style={{ width: '500px', display: "inline-block", verticalAlign: 'top' }}>
+                <table className={classNames.board}>
+                    <tbody>
+                        {
+                            board.map((row: Square[], y: number) => {
+                                return (
+                                    <tr key={y}>
+                                        {
+                                            row.map((square: Square, x: number) => {
+                                                return <td key={x} className={classNames.cell}>
+                                                    <div className={classNames.square + " " + resolveTheme(square.color).squareColor + " " + (square.highlight ? classNames.highlight : '') + " " + (selectedPiece.x === x && selectedPiece.y === y ? classNames.currentSelection : "")} onClick={() => { handleSquareClick(square, x, y) }}>
+                                                        {square.piece ? square.piece.render() : null}
+                                                    </div>
+                                                </td>
+                                            })
+                                        }
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <div style={{ display: "inline-block" }}>
+                <div>
+                    {gameOver ?
+                        <>
+                            <h1>Game Over</h1>
+                            <h2>Winner : {turn !== Color.white ? "White" : "Black"}</h2>
+                        </>
+                        :
+                        <div>
+                            Current Player : {turn === Color.white ? "White" : "Black"}
+                        </div>}
+                </div>
+
+                <table>
+                    <tbody>
+                        <tr>
+                            {blackKilled.map((dead: Piece, index: number) => {
+                                return <td key={index}>
+                                    {dead.render()}
+                                </td>
+                            })}
+                        </tr>
+                    </tbody>
+                </table>
+                <table>
+                    <tbody>
+                        <tr>
+                            {whiteKilled.map((dead: Piece, index: number) => {
+                                return <td key={index}>
+                                    {dead.render()}
+                                </td>
+                            })}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
